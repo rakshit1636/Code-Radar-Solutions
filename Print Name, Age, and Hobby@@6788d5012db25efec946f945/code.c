772e9cbedbd5f2803c6b3d327f87877a88f1dd05
rakshit1636 <rakshit1636.becse24@chitkara.edu.in>
2025-02-03 13:46:39 +0530

    Solution to Print Name, Age, and Hobby

--- Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
+++ Print Name, Age, and Hobby@@6788d5012db25efec946f945/code.c
@@ -5,24 +5,21 @@ int main() {
     char name[50], hobby[50];
     int age;
 
-    // Taking input
-    printf("Enter your name: ");
+    printf("");
     scanf("%s", name);
 
-    printf("Enter your age: ");
+    printf("");
     scanf("%d", &age);
-    getchar(); // Consume the leftover newline character
+    getchar(); 
 
-    printf("Enter your hobby: ");
+    printf("");
     fgets(hobby, sizeof(hobby), stdin);
 
-    // Removing newline character from hobby if present
     size_t len = strlen(hobby);
     if (len > 0 && hobby[len - 1] == '\n') {
         hobby[len - 1] = '\0';
     }
 
-    // Printing output
     printf("\nName: %s\n", name);
     printf("Age: %d\n", age);
     printf("Hobby: %s\n", hobby);
